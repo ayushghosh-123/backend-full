0dad1ff80da812850b3c7e9519062a066b3765ec
Ayush ghosh <ghoshayush910@gmail.com>
2025-03-17 02:28:17 +0530

    node module chal rah hai

--- backend - 1/Installnode.js/script.js
+++ backend - 1/Installnode.js/script.js
@@ -7,7 +7,48 @@ rename file
 unlike
  */
 
-fs.copyFile("hello.txt", "./Copy/copy.txt", (err)=>{
-    if(err) console.log(err.message)
-    else console.log("done")
-})
+// fs.copyFile("hello.txt", "./Copy/copy.txt", (err)=>{
+//     if(err) console.log(err.message)
+//     else console.log("done")
+// })
+
+// fs.unlink("hello.txt", (err) =>{
+//     if(err) console.error(err)
+//     else console.log("removed")
+// })
+
+// fs.rm("./copy", {recursive: true}, (err) =>{
+//     if(err) console.error(err)
+//     else console.log("removed")
+// })
+
+// fs.mkdir("./copy", {recursive: true}, (err) => {
+//     if(err) console.error(err)
+//     else console.log("create")
+// })
+
+// fs.writeFile('./copy/ayush.txt', "hello everyone", err => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//         console.log("done")
+//     }
+//   });
+
+// fs.appendFile('./copy/ayush.txt', "hello Ayush ", err => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//         console.log("done")
+//     }
+//   });
+
+fs.readFile('./copy/ayush.txt', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+        console.log(data)
+    }
+  });
+
+
