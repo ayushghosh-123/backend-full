4887851e704d226a7675022824f3fd165d6d1d83
Ayush ghosh <ghoshayush910@gmail.com>
2025-03-20 17:31:17 +0530

    HSC chal raha hai

---
+++ backend - 1/HSC/Script.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+
+app.use((req, res , next) =>{
+    console.log('middleware challao ....')
+    next()
+});
+
+app.use((req, res , next) =>{
+    console.log('middleware challao aur ek bar ....')
+    next()
+});
+
+
+
+app.get("/about", (req, res) =>{
+    res.send("about chalao")
+})
+
+
+// rotes create karna
+app.get("/", (req, res, next) =>{
+    // res.send("champion meraaaa annnnuj")
+    return next(new Error("something went wrong"))
+})
+
+// error handler
+app.use((err, req , res, next) =>{
+    console.error(err.stack)
+    res.status(500).send("something broke...")
+})
+
+// make the server
+app.listen(3000)
